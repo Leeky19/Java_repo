28ddc8cea6e02230fad983601dcbb718e8382c0d
Julien <arzourjulien@gmail.com>
2023-07-10 16:37:44 +0200

    ajout du premier exemple

---
+++ exemple/exemple_n1.java
@@ -0,0 +1,11 @@
+public class Programme {
+    //variable de classe
+
+    //main methode, static 
+    public static void main (String[] args) {
+        System.out.prinln("Hello World")
+        //variable locales
+    }
+}
+
+
