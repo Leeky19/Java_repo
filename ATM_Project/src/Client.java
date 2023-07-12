72a9a186d15bc30b22280502d971b26351cdbd3e
Julien <arzourjulien@gmail.com>
2023-07-12 16:34:40 +0200

    Ajout et test de la classe Client

--- ATM_Project/src/Client.java
+++ ATM_Project/src/Client.java
@@ -1,3 +1,51 @@
 public class Client {
+    private String nom;
+    private String prenom;
+    private String  adresse;
+    private int codePostal;
+    private String ville;
+
+    public Client(String nom, String prenom, String adresse, int codePostal, String ville) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.codePostal = codePostal;
+        this.ville = ville;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+    public int getCodePostal() {
+        return codePostal;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public void setPrenom(String prenom) {
+        this.nom = prenom;
+    }
+    public void setAdresse(String adresse){
+        this.adresse = adresse;
+    }
+    public void setCodePostal(int codePostal){
+        this.codePostal = codePostal;
+    }
+    public void setVille(String ville){
+        this.ville = ville;
+    }
 
 }
--- ATM_Project/src/Main.java
+++ ATM_Project/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Compte compte = new Compte("1234567890");
+       /* Partie compte
+       Compte compte = new Compte("1234567890");
 
         System.out.println("Numéro de compte : " + compte.getNumero());
         System.out.println("Solde initial : " + compte.getSolde());
@@ -15,5 +16,31 @@ public class Main {
 
         compte.deposer(200.0);
         System.out.println("Solde après nouveau dépôt : " + compte.getSolde());
+
+        */
+
+        //creer un nouveau client
+        Client client1 = new Client("Dupont", "Jean", "10 Rue de la Paix", 75001, "Paris");
+
+        //Initialisation des données du client
+        System.out.println("Nom : " + client1.getNom());
+        System.out.println("Prénom : " + client1.getPrenom());
+        System.out.println("Adresse : " + client1.getAdresse());
+        System.out.println("Code postal : " + client1.getCodePostal());
+        System.out.println("Ville : " + client1.getVille() + "\n");
+
+        //Changent dans les données
+        client1.setNom("Dupon");
+        client1.setPrenom("Janne");
+        client1.setAdresse("456 Rue de la Sainte Rép");
+        client1.setCodePostal(69890);
+        client1.setVille("Tagalatchointchoin");
+
+        //affiche les nouvelles données
+        System.out.println("Nouveau nom : " + client1.getNom());
+        System.out.println("Nouveau prénom : " + client1.getPrenom());
+        System.out.println("Nouvelle adresse : " + client1.getAdresse());
+        System.out.println("Nouveau code postal : " + client1.getCodePostal());
+        System.out.println("Nouvelle ville : " + client1.getVille());
     }
 }
